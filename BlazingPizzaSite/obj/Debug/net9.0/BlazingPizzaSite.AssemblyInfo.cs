9220b88055593c0ec12f5f1cea55972d3c883f98
hallegracecooper <dri19003@byui.edu>
2025-09-29 16:25:24 -0600

    pt1

--- BlazingPizzaSite/obj/Debug/net9.0/BlazingPizzaSite.AssemblyInfo.cs
+++ BlazingPizzaSite/obj/Debug/net9.0/BlazingPizzaSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb4c13476b1c7950e99999300b095add188dba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402d389facdb5f5b996ef8443228ae0918f41b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
